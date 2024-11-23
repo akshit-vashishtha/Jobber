4c5a9141c4d692cd687f0ea99b20e927b73f046a
akshit-vashishtha <akshitv322@gmail.com>
2024-11-23 11:18:59 +0530

    basic header done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import Header from "./components/Header"
 export default function App() {
   return (
     <>
-    <Header/>
-    <h1 className="bg-slate-500"> hi</h1>
+      <Header/>
+
     </>
   )
 }
