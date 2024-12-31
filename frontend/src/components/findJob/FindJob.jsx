7331832f0bdccf9e77514baabca54258e63b82c6
akshit-vashishtha <akshitv322@gmail.com>
2024-12-31 12:39:48 +0530

    small changes

--- frontend/src/components/findJob/FindJob.jsx
+++ frontend/src/components/findJob/FindJob.jsx
@@ -7,9 +7,9 @@ export default function FindJob() {
   const [searchQuery, setSearchQuery] = useState('');
   return (
     <div className='h-screen mt-[1%] ml-[2%] mr-[2%]'>
-        <div className=''>
+        {/* <div className=''>
             <h1 className='text-6xl'>Find Your Ideal Job!</h1>
-        </div>
+        </div> */}
         <div className='searchSection'>
             <SearchSection searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
         </div>
